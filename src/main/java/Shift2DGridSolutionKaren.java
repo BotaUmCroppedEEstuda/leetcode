82f31be3b37c38f7409e9611562ed17c63f64915
Provider <leticia.martin@provider-it.com.br>
2022-07-25 11:32:39 -0300

    1260. Shift 2D Grid

---
+++ src/main/java/Shift2DGridSolutionKaren.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Shift2DGridSolutionKaren {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        List<List<Integer>> newGrid = new ArrayList<>();
+
+        Deque<Integer> auxQueue = new LinkedList<>();
+        for (int[] line : grid) {
+            for (int element : line) {
+                auxQueue.add(element);
+            }
+        }
+        while (k != 0) {
+            if (k < 0) {
+                auxQueue.addLast(auxQueue.pollFirst());
+                k++;
+            } else {
+                auxQueue.addFirst(auxQueue.pollLast());
+                k--;
+            }
+        }
+        for (int line = 0; line < grid.length; line++) {
+            List<Integer> returnLines = new ArrayList<>();
+            for (int column = 0; column < grid[line].length; column++) {
+                returnLines.add(auxQueue.pollFirst());
+            }
+            newGrid.add(returnLines);
+        }
+        return newGrid;
+    }
+}
---
+++ src/main/java/Shift2DGridSolutionLeticia.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Shift2DGridSolutionLeticia {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> response = new ArrayList<>();
+
+        if (grid.length == 0) {
+            return response;
+        }
+        Deque<Integer> lista = new LinkedList<>();
+
+        for (int [] linha : grid) {
+            for (int elemento : linha) {
+                lista.add(elemento);
+            }
+        }
+
+        while (k != 0) {
+            if (k > 0) {
+                Integer elemento = lista.getLast();
+                lista.addFirst(elemento);
+                lista.removeLast();
+                k--;
+            } else {
+                Integer elemento = lista.getFirst();
+                lista.addLast(elemento);
+                lista.removeFirst();
+                k++;
+            }
+        }
+
+        List<Integer> listaResposta = new ArrayList<>();
+        listaResposta.addAll(lista);
+
+        int tamanhoInicialLista = 0;
+        int quantidadeItensLinha = grid[0].length;
+        int tamanhoFinalLista = quantidadeItensLinha;
+
+        while (tamanhoFinalLista <= grid.length * quantidadeItensLinha) {
+            response.add(listaResposta.subList(tamanhoInicialLista, tamanhoFinalLista));
+            tamanhoInicialLista = tamanhoFinalLista;
+            tamanhoFinalLista = tamanhoFinalLista + quantidadeItensLinha;
+        }
+
+        return response;
+    }
+}
